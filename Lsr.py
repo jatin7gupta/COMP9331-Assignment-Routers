ddac98705e733a40edcab6c37b5f79bc5c390a30
Jatin Gupta <guptajatin@outlook.com>
2019-07-25 08:49:34 +1000

    Added test.py and commented

--- Lsr.py
+++ Lsr.py
@@ -1,9 +1,15 @@
 import sys
 import pdb
 
+ARGS_NUMBER = 2
+FILE_NAME = 1
+ROUTER_NAME = 0
+PARENT_PORT = 1
+CHILD_PORT = 2
+DISTANCE = 1
+
 
 class Router:
-
     def __init__(self, name, port, neighbours_list):
         self.name = name
         self.port = port
@@ -13,8 +19,15 @@ class Router:
         self.neighbours.append(neighbour)
 
 
-if len(sys.argv) == 2:
-    f = open(sys.argv[1], "r")
+class Neighbours:
+    def __init__(self, name, port, distance):
+        self.name = name
+        self.port = port
+        self.distance = distance
+
+
+if len(sys.argv) == ARGS_NUMBER:
+    f = open(sys.argv[FILE_NAME], "r")
     line_counter = 0
     number_of_neighbour = 0
     # pdb.set_trace()
@@ -23,15 +36,20 @@ if len(sys.argv) == 2:
     for line in f:
         list_file.append(line.split())
     for i in range(len(list_file)):
+        # First line will always be Parent router
         if i == 0:
-            parent_router = Router(list_file[i][0], list_file[i][1], [])
+            parent_router = Router(list_file[i][ROUTER_NAME], list_file[i][PARENT_PORT], [])
+
+        # Second line will always be the number of neighbours
         elif i == 1:
-            number_of_neighbour = list_file[i]
+            number_of_neighbour = list_file[i]  # TODO not using right now
+
+        # From 3 onwards it will be the child routers
         elif i > 1:
-            child_router = Router(list_file[i][0], list_file[i][2], [])
+            child_router = Neighbours(list_file[i][ROUTER_NAME], list_file[i][CHILD_PORT], list_file[i][DISTANCE])
             parent_router.add_neighbour(child_router)
         line_counter += 1
-    print(parent_router)
+
     for i in parent_router.neighbours:
-        print(i.name)
+        print('Parent name = ', parent_router.name, 'child name->', i.name)
 
