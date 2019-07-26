398c059de74ab4ed7952d33a85b7c6a62556d2e2
Jatin Gupta <guptajatin@outlook.com>
2019-07-27 08:39:21 +1000

    Modified: threading and added new parser

--- Lsr.py
+++ Lsr.py
@@ -2,6 +2,8 @@ import sys
 import socket as s
 import time
 import concurrent.futures
+import json
+import threading
 import pdb
 
 ARGS_NUMBER = 2
@@ -10,8 +12,34 @@ ROUTER_NAME = 0
 PARENT_PORT = 1
 CHILD_PORT = 2
 DISTANCE = 1
-UPDATE_INTERVAL = 1
+UPDATE_INTERVAL = 50
 SERVER_NAME = 'localhost'
+
+
+def dict_to_obj(our_dict):
+    """
+    Function that takes in a dict and returns a custom object associated with the dict.
+    This function makes use of the "__module__" and "__class__" metadata in the dictionary
+    to know which object type to create.
+    """
+    if "__class__" in our_dict:
+        # Pop ensures we remove metadata from the dict to leave only the instance arguments
+        class_name = our_dict.pop("__class__")
+
+        # Get the module name from the dict and import it
+        module_name = our_dict.pop("__module__")
+
+        # We use the built in __import__ function since the module name is not yet known at runtime
+        module = __import__(module_name)
+
+        # Get the class from the module
+        class_ = getattr(module, class_name)
+
+        # Use dictionary unpacking to initialize the object
+        obj = class_(**our_dict)
+    else:
+        obj = our_dict
+    return obj
 
 
 def convert_to_dict(obj):
@@ -19,7 +47,6 @@ def convert_to_dict(obj):
     A function takes in a custom object and returns a dictionary representation of the object.
     This dict representation includes meta data such as the object's module and class names.
     """
-
     #  Populate the dictionary with object meta data
     obj_dict = {
         "__class__": obj.__class__.__name__,
@@ -48,7 +75,7 @@ class Router:
 
 class Message:
     def __init__(self, sender: Router):
-        self.port = sender.port
+        # self.port = sender.port
         self.name = sender.name
         self.neighbours = sender.neighbours
         self.sequence_number = 0
@@ -65,23 +92,38 @@ class Neighbours:
 
 
 def udp_client(_parent_router):
+    # this client will have 2 tasks
+    # 1. send my message to the child
+    # 2. forward the message received, to my child by checking if I have not sent it previously
+
+    client_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    # TODO
+    # client_socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+    # client_socket.bind((SERVER_NAME, int(_parent_router.port)))
     while True:
         for child in _parent_router.neighbours:
-            message_to_send = f'hello router {child.name} I am your parent {parent_router.name}'
+            message_to_send = json.dumps(_parent_router.message, default=convert_to_dict)
             server_port = int(child.port)
-            client_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
             client_socket.sendto(str.encode(message_to_send), (SERVER_NAME, server_port))
-            client_socket.close()
         time.sleep(UPDATE_INTERVAL)
 
 
 def udp_server(_parent_router):
     server_port = int(_parent_router.port)
     server_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    server_socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
     server_socket.bind((SERVER_NAME, server_port))
     while True:
         message, client_address = server_socket.recvfrom(2048)
-        print(f'I am server {_parent_router.name}, I have recieved data= {message} from port {client_address}')
+        str_ = message.decode("utf-8")
+        #obj = json.loads(str_, object_hook=dict_to_obj)
+        print(str_)
+
+
+
+
+
+        # print(f'I am server {_parent_router.name}, I have recieved data= {message} from port {client_address}')
 
 
 if len(sys.argv) == ARGS_NUMBER:
@@ -109,10 +151,16 @@ if len(sys.argv) == ARGS_NUMBER:
         line_counter += 1
 
     parent_router.add_message(Message(parent_router))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(udp_client, parent_router)
-        executor.submit(udp_server, parent_router)
-        executor.shutdown()
+
+    client_thread = threading.Thread(target=udp_client, args=(parent_router,))
+    server_thread = threading.Thread(target=udp_server, args=(parent_router,))
+    client_thread.start()
+    server_thread.start()
+
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    #     executor.submit(udp_client, parent_router)
+    #     executor.submit(udp_server, parent_router)
+    #     executor.shutdown()
 
 
 
